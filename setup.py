7f4e293db3051a70399730e7ee651d1fc4104f52
Mathieu Lecarme <mathieu@garambrogne.net>
2012-04-04 21:20:23 +0200

    setup file.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='imapidle',
+    version='0.1',
+    package_dir={'': 'src'},
+    url='https://github.com/athoune/imapidle',
+    description='Handling IMAP IDLE command',
+    long_description='''
+    ''',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Operating System :: POSIX',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Communications :: Email'
+        ],
+    license='MIT',
+    author='Mathieu Lecarme',
+    packages=[],
+    keywords=['imap'],
+    zip_safe=True,
+    install_requires=[]
+        )
